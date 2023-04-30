5ac7da4880d761b6f5bc719a3df0f7124731f415
Alain Georges Schaerer <alainsch@buffalo.edu>
2023-04-30 12:53:10 -0400

    volume and battery

--- blocks.def.h
+++ blocks.def.h
@@ -4,6 +4,8 @@ static const Block blocks[] = {
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"Bat: ", "echo $(upower -i $(upower -e | grep 'BAT') | grep -E 'percentage' | awk '{print $2}')", 30, 0},
+	{"Vol: ", "amixer -c 1 get Master | grep -o '[0-9]*%'", 10, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
